fbdbbc0f0ad81e0efc601482b794575a6728a2b4
Hazeqdaniel <hazeqwan01@gmail.com>
2025-01-29 19:17:29 +0800

    nav_again

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import Nav from "./components/nav"
+import Main from "./components/main"
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold'>
-    <Nav/>
-  </h1>
+      <Main/>
     </>
   )
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,20 @@
-
+import logo from '../assets/hzqDnl.svg'
 
 const nav = () => {
   return (
-    <div>
-      nav
-    </div>
+    <>
+    <nav className="flex justify-between font-mono rounded-bl-2xl rounded-br-2xl mx-40 px-16 items-center h-16 bg-emerald-200 shadow-2xl relative" role="navigation">
+      <div className="w-3/6">
+      <img src={logo} alt="logo" className='w-20'/>
+      </div>
+      <ul className="flex w-3/6 space-x-6 items-center justify-end">
+        <li className="hover:bg-emerald-400 hover:text-white rounded-2xl p-2"><a href="#">About Me</a></li>
+        <li className="hover:bg-emerald-400 hover:text-white rounded-2xl p-2"><a href="#">Projects Involved</a></li>
+        <li className="hover:bg-emerald-400 hover:text-white rounded-2xl p-2"><a href="#">Languages</a></li>
+        <li className="hover:bg-emerald-400 hover:text-white rounded-2xl p-2"><a href="#">Send Details</a></li>
+      </ul>
+    </nav>
+    </>
   )
 }
 
